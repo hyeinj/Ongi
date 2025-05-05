b263ae389326e8b723169d78648c21067b700290
고병찬 <70642609+byungchanKo99@users.noreply.github.com>
2025-05-05 17:13:14 +0900

    [frontend] 초기 설정
    - nextjs로 하기로함
    - 왜냐하면 mvp 개발 시 서버 액션으로 간단한 ai api 호출하기 위함.
    - vercel로 배포 간단히 하기 위함도 있음

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '따온',
+  description: '따온',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
