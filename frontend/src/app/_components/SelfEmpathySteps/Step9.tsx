b6ef745dd3bdadfe52b4fc76fc5f41d724fc316a
JibinHwang <jibin.hwang@gmail.com>
2025-05-19 03:02:32 +0900

    [frontend] fix: 글씨체 수정하며 바뀐 레이아웃 수정

--- frontend/src/app/_components/SelfEmpathySteps/Step9.tsx
+++ frontend/src/app/_components/SelfEmpathySteps/Step9.tsx
@@ -13,7 +13,7 @@ export default function Step9() {
 return (
     <SelfEmpathyLayout currentStep={9} onBack={() => router.push('/self-empathy/8')}>
         <span className="next-text">무지님의 소중한 오늘을 살펴보았어요.<br /><br />이제 무지님과 닮은 마음에<br /> 조심스런 답장을 건네러 가볼까요?</span>
-        <button className="bottom-button final-button"onClick={() => router.push('/self-empathy/10')}> 
+        <button className="bottom-button final-button"onClick={() => router.push('/letter-exercise/1')}> 
             {/* 병찬님 화면으로 넘어가게 */}
           <Image src={bottomButton} alt="다음으로" />
           <span className="button-text final-button-text">마음을 담아 건네러 갈게요</span>
