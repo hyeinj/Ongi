b7867792a2d3549685c796f667dca409a25950f3
hyeinj <jhi2002@hanyang.ac.kr>
2025-06-15 17:04:16 +0900

    아카이빙 report 조회 api 개발 완료

--- backend/src/main/java/com/ongi/backend/Repository/ReportRepository.java
+++ backend/src/main/java/com/ongi/backend/Repository/ReportRepository.java
@@ -5,10 +5,12 @@ import com.ongi.backend.Entity.SelfEmpathy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
     List<Report> findBySelfEmpathy(SelfEmpathy selfEmpathy);
+    List<Report> findByIslandAndCreatedAtBefore(Integer island, LocalDateTime beforeDate);
 }
